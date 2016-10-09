1929a66552a05556ed282c24563c141120925b78
AO Industries, Inc. <support@aoindustries.com>
2016-10-09 16:03:20 -0500

    Set top comment correct for this submodule name.

--- src/main/java/com/aoindustries/dao/dbc/QueryReport.java
+++ src/main/java/com/aoindustries/dao/dbc/QueryReport.java
@@ -1,24 +1,24 @@
 /*
- * ao-dao - Simple data access objects framework.
- * Copyright (C) 2011, 2013, 2015  AO Industries, Inc.
+ * ao-dao-dbc - Simple data access objects framework implementation leveraging ao-dbc.
+ * Copyright (C) 2011, 2013, 2015, 2016  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
  *
- * This file is part of ao-dao.
+ * This file is part of ao-dao-dbc.
  *
- * ao-dao is free software: you can redistribute it and/or modify
+ * ao-dao-dbc is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * ao-dao is distributed in the hope that it will be useful,
+ * ao-dao-dbc is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ao-dao.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ao-dao-dbc.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.aoindustries.dao.dbc;
 
