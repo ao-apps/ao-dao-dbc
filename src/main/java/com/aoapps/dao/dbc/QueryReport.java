b3366543fc58aae648e9e49948f68c5adc243536
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:49:24 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/dao/dbc/QueryReport.java
+++ src/main/java/com/aoapps/dao/dbc/QueryReport.java
@@ -1,6 +1,6 @@
 /*
  * ao-dao-dbc - Simple data access objects framework implementation leveraging ao-dbc.
- * Copyright (C) 2011, 2013, 2015, 2016, 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2011, 2013, 2015, 2016, 2019, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-dao-dbc.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.dao.dbc;
 
 import com.aoapps.collections.AoCollections;
